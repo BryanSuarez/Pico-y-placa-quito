9c9ecf5c4d35c8790bb46cc2aed62235118822a3
BRYANSUAREZ\BRYAN SUAREZ <bryanbolivar.1992@gmail.com>
2018-02-21 21:49:31 -0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-12 ">
+            <h2 class="text-center">Bienvenidos al Sistema de Consultas de Pico y Placa</h2>
+            <form action="">
+                
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
