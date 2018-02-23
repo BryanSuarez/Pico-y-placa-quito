e17ace314e54a333f1d51c544179e9a1eca6e100
BRYANSUAREZ\BRYAN SUAREZ <bryanbolivar.1992@gmail.com>
2018-02-22 19:50:57 -0500

    New route type post, to check availability

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,22 +1,37 @@
-<form>
+<form action="{{ route('consult') }}" method="POST">
+    {{ csrf_field() }}
     <div class="form-group">
         <label for="placa">N° de Placa</label>
-        <input type="text" class="form-control" id="" placeholder="Ej: PAA4276">
+        <input type="text" class="form-control" name="license_plate" placeholder="Ej: PAA4276">
     </div>
     <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="col-sm-12 col-md-4 col-lg-4">
             <div class="form-group">
                 <label for="placa">Fecha (Día):</label>
-                <input type="text" class="form-control" id="" placeholder="Ej: Lunes">
+                <input type="text" class="form-control" name="day" placeholder="Ej: Lunes">
             </div>
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="col-sm-12 col-md-4 col-lg-4">
             <div class="form-group">
                 <label for="placa">Hora:</label>
-                <input type="time" class="form-control">
+                <input type="time" class="form-control" name="time">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4">
+            <div class="form-group">
+                <div class="">
+                    <label for="placa">Tipo de matrícula:</label><br>
+                    <label>
+                      <input type="radio" name="type" value="public"> Pública
+                    </label>
+                    <label>
+                      <input type="radio" name="type" value="private"> Particular
+                    </label>
+              </div>
             </div>
         </div>
     </div>
+
     <div class="row">
         <div class="col-sm-12 col-md-3 col-lg-6 col-md-offset-1">
             <div class="form-group">
