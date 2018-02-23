8b56e87f457c18bfd8b510163a4455487768a5d1
BRYANSUAREZ\BRYAN SUAREZ <bryanbolivar.1992@gmail.com>
2018-02-22 19:25:30 -0500

    Main view created

---
+++ resources/views/partials/form.blade.php
@@ -0,0 +1,32 @@
+<form>
+    <div class="form-group">
+        <label for="placa">N° de Placa</label>
+        <input type="text" class="form-control" id="" placeholder="Ej: PAA4276">
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <div class="form-group">
+                <label for="placa">Fecha (Día):</label>
+                <input type="text" class="form-control" id="" placeholder="Ej: Lunes">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <div class="form-group">
+                <label for="placa">Hora:</label>
+                <input type="time" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-3 col-lg-6 col-md-offset-1">
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Generar Consulta</button>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-4 col-md-offset-1">
+            <div class="form-group">
+                <button type="submit" class="btn btn-danger">Nueva Consulta</button>
+            </div>
+        </div>
+    </div>
+</form>
