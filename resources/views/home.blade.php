d2494ce9631797305f0a5ef138458fd26030ab94
BRYANSUAREZ\BRYAN SUAREZ <bryanbolivar.1992@gmail.com>
2018-02-22 19:49:25 -0500

    Home view to displaying the form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading text-center">
+                    <h3 class="text-center">Consulta de movilidad de autos y motos en Quito</h3>
+                </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,7 +16,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h4 class="text-center">Ingrese los datos indicados</h3>
+                    @include('partials.form')
                 </div>
             </div>
         </div>
